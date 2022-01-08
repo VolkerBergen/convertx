09dac0973e1614c2b04580e9bbc333c59f0426a9
VolkerBergen <email@volkerbergen.de>
2022-01-08 11:00:06 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='convertx',
-    version='0.0.1',
+    version='0.1',
     description='Convert Word documents (docx) to clean HTML and Markdown',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     author='Volker Bergen',
